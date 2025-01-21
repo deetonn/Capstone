2cd7ceeacbb80a230e4b6737b594445170ecad69
deetonn <deetonrushton@gmail.com>
2025-01-21 19:12:09 +0000

    Main project stuff

--- codeinstitute/urls.py
+++ codeinstitute/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='/blog/', permanent=True)),  # Redirect root to /blog/
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
