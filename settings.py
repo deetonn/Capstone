d22b6d21d0b8885907c18d5bcf97892ec9f3334b
deetonn <deetonrushton@gmail.com>
2025-01-22 09:51:14 +0000

    Settings: get ready for heroku

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+import django_heroku
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -17,4 +19,9 @@ TEMPLATES = [
             ],
         },
     },
-] 
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals()) 
