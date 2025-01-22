e852dc7178b2f769d8ff92b59854b4aed7bc2ab0
deetonn <deetonrushton@gmail.com>
2025-01-22 09:58:12 +0000

    Settings: more readiness for heroku

--- settings.py
+++ settings.py
@@ -24,4 +24,6 @@ TEMPLATES = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+ALLOWED_HOSTS = ['*.herokuapp.com']
+
 django_heroku.settings(locals()) 
