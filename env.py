d0b40291bcf0c3c7d0854b986c04babc6d221c03
Deeton Rushton <76126281+deetonn@users.noreply.github.com>
2025-01-28 14:10:14 +0000

    Update env.py

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ import os
 os.environ['SECRET_KEY'] = 'your-secret-key'
 os.environ['DEVELOPMENT'] = 'True'
 # Set because I am using the CI db locally (Bad?? maybe idc)
-os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:3tgNshSPlwz6@ep-steep-sunset-a28pigfk.eu-central-1.aws.neon.tech/daisy_pound_woven_483693'
+os.environ['DATABASE_URL'] = 'redacted-from-github'
